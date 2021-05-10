3bdfd2ac4d661674476d7e3dc779ecffe78862a3
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2021-05-10 16:34:04 +0900

    Fix setting random seed for generating random value at Wio Terminal.

--- src/M5bit-less/M5bit-less.ino
+++ src/M5bit-less/M5bit-less.ino
@@ -471,6 +471,7 @@ void setup() {
   esp_efuse_mac_get_default(mac0);
 #else
   // Create random mac address for avoid conflict ID.
+  randomSeed(analogRead(A0));
   for (int i = 0; i < sizeof(mac0); i++) {
     mac0[i] = random(256);
   }
