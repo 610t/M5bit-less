1eac11b07494832a03e843df2fd2d399b97b40eb
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2024-01-03 21:53:31 +0900

    Use PWM output for blinking Wio Terminal LED.

--- src/M5bit-less/M5bit-less.ino
+++ src/M5bit-less/M5bit-less.ino
@@ -669,11 +669,7 @@ class CmdCallbacks : public BLECharacteristicCallbacks {
 #if !defined(ARDUINO_WIO_TERMINAL)
       M5.Power.setLed(constrain(data_val, 0, 255));
 #else
-      if (constrain(data_val, 0, 255) >= 128) {
-        digitalWrite(LED_BUILTIN, HIGH);
-      } else {
-        digitalWrite(LED_BUILTIN, LOW);
-      }
+      analogWrite(LED_BUILTIN, constrain(data_val, 0, 255));
 #endif
     }
 
